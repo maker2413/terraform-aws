1055b02fe827be05c4193fda00ebfe4d24e7db6b
maker2413 <maker2413@gmail.com>
2021-02-18 23:26:53 -0800

    improved subnet code

--- networking/main.tf
+++ networking/main.tf
@@ -5,6 +5,11 @@ resource "random_integer" "random" {
   max = 100
 }
 
+resource "random_shuffle" "az_list" {
+  input = data.aws_availability_zones.available.names
+  result_count = var.max_subnets
+}
+
 resource "aws_vpc" "squids_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
@@ -20,7 +25,7 @@ resource "aws_subnet" "squids_public_subnet" {
   vpc_id                  = aws_vpc.squids_vpc.id
   cidr_block              = var.public_cidrs[count.index]
   map_public_ip_on_launch = true
-  availability_zone       = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"][count.index]
+  availability_zone       = random_shuffle.az_list.result[count.index]
 
   tags = {
     Name = "squids_vpc_public${count.index + 1}"
@@ -32,7 +37,7 @@ resource "aws_subnet" "squids_private_subnet" {
   vpc_id                  = aws_vpc.squids_vpc.id
   cidr_block              = var.private_cidrs[count.index]
   map_public_ip_on_launch = false
-  availability_zone       = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"][count.index]
+  availability_zone       = random_shuffle.az_list.result[count.index]
 
   tags = {
     Name = "squids_vpc_private_${count.index + 1}"
